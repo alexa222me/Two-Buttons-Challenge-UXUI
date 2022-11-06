a697e3b91bb99c2d2a202c3f54930d0f7a6deb84
Alexa Mendoza <alexandra96me@hotmail.com>
2022-11-05 19:41:03 -0500

    Initial Commit

---
+++ Two Buttons Challenge UXUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Buttons Challenge UXUI
+//
+//  Created by Alexa Mendoza on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
