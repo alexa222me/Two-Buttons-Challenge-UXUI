c7623c75094bf870488a34b8e4018b4089d0499e
Alexa Mendoza <alexandra96me@hotmail.com>
2022-11-05 20:01:20 -0500

    Made two buttons work

--- Two Buttons Challenge UXUI/ViewController.swift
+++ Two Buttons Challenge UXUI/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessagePressedA(_ sender: UIButton) {
+        messageLabel.text = "YOU ARE AWESOME!!"
+        
+    }
+    
+    @IBAction func showMessageB(_ sender: UIButton) {
+        messageLabel.text = "YOU ARE GREAT!"
+    }
+    
 
 }
 
