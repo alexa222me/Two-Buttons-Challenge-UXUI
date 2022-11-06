2c3e75d0cd4815b7a40b6012686c5bda718990b6
Alexa Mendoza <alexandra96me@hotmail.com>
2022-11-05 20:17:15 -0500

    Fixed alignment

--- Two Buttons Challenge UXUI/ViewController.swift
+++ Two Buttons Challenge UXUI/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController {
     @IBAction func showMessagePressedA(_ sender: UIButton) {
         messageLabel.text = "YOU ARE AWESOME!"
         messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
         
     }
     
     @IBAction func showMessageB(_ sender: UIButton) {
         messageLabel.text = "YOU ARE GREAT!"
         messageLabel.textColor = UIColor.red
+        messageLabel.textAlignment = .right
     }
     
 
